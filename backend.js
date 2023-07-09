330728a07131bd27a7af67ddf9128c81dbff0221
ReWhiteZ <phurikung290948@gmail.com>
2023-07-09 09:22:37 +0700

    Add Checked answer

--- backend.js
+++ backend.js
@@ -7,6 +7,13 @@ function entername(){
     }
 }
 
+function checkanswer(qc){
+    for(var i = 0; i < qc.length; i++){
+        if(qc[i].checked) return qc[i].value;
+    }
+}
+
+
 document.addEventListener('DOMContentLoaded', function(){
     document.getElementById("getname").innerHTML = localStorage.getItem("Name");
 
@@ -14,10 +21,23 @@ document.addEventListener('DOMContentLoaded', function(){
     form.addEventListener('submit', function(event){
         event.preventDefault();
 
-        const q4 = document.querySelector('input[name="4"]:checked').value;
+        var qc1 = document.getElementsByName("1");
+        var qc2 = document.getElementsByName("2");
+        var qc3 = document.getElementsByName("3");
+        var qc4 = document.getElementsByName("4");
 
-        if(q4 == "อยาก") location.href = 'endwant.html'
-        else location.href = 'enddont.html'
+        var q1 = checkanswer(qc1);
+        var q2 = checkanswer(qc2);
+        var q3 = checkanswer(qc3);
+        var q4 = checkanswer(qc4);
+        
+        if(q1 == null || q2 == null || q3 == null || q4 == null) alert("Please complete the question!!");
+        else {
+            const a4 = document.querySelector('input[name="4"]:checked').value;
+
+            if(a4 == "อยาก") location.href = 'endwant.html'
+            else if(a4 == "ไม่อยาก") location.href = 'enddont.html'
+        }
     });
 });
 
